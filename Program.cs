561d2296f0201751743c0c503e2aa32f7ad975b2
JuanBr100 <juanrdo2009@hotmail.com>
2024-07-03 20:03:23 -0300

    Seeding Service

--- Program.cs
+++ Program.cs
@@ -11,16 +11,32 @@ builder.Services.AddDbContext<SalesWebMvcContext>(options =>
 // Adicionar serviços ao contêiner
 builder.Services.AddControllersWithViews();
 
+
+
+
 var app = builder.Build();
 
 // Configurar o pipeline de requisição HTTP
 if (!app.Environment.IsDevelopment())
 {
+   
     app.UseExceptionHandler("/Home/Error");
     // O valor padrão do HSTS é 30 dias. Você pode querer mudar isso para cenários de produção, veja https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{   // <-- este bloco inteiro de else
+    using (var scope = app.Services.CreateScope())
+    {
+        var services = scope.ServiceProvider;
+        var context = services.GetRequiredService<SalesWebMvcContext>();
+        var seedingService = new SeedingService(context);
+        seedingService.Seed();
+    }
+}
 
+//Popular as tabelas do sistema
+//app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
