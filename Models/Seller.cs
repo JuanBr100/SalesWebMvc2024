86521eb7ddca990880604573e8fc77605e902c4c
JuanBr100 <juanrdo2009@hotmail.com>
2024-07-03 19:08:27 -0300

    Other entities and second migrations

---
+++ Models/Seller.cs
@@ -0,0 +1,42 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string  name { get; set; }
+        public string email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary, Department department)
+        {
+            Id=id;
+            this.name=name;
+            this.email=email;
+            BirthDate=birthDate;
+            BaseSalary=baseSalary;
+            Department=department;
+        }
+
+        public void AddSales(SalesRecord record)
+        {
+            Sales.Add(record);
+        }
+
+        public void RemoveSales(SalesRecord salesRecord)
+        {
+            Sales.Remove(salesRecord);  
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
