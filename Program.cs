83dca83c6c7923dac02f6763317ef4d861bead6a
JuanBr100 <juanrdo2009@hotmail.com>
2024-07-04 20:27:52 -0300

    SellerService and basic Findall

--- Program.cs
+++ Program.cs
@@ -1,47 +1,52 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure o contexto do banco de dados
 builder.Services.AddDbContext<SalesWebMvcContext>(options =>
     options.UseMySql(builder.Configuration.GetConnectionString("SalesWebMvcContext") ?? throw new InvalidOperationException("Connection string 'SalesWebMvcContext' not found."),
     new MySqlServerVersion(new Version(8, 0, 26)))); // Especificar a versão do MySQL
 
 // Adicionar serviços ao contêiner
 builder.Services.AddControllersWithViews();
-
-
-
+builder.Services.AddTransient<SeedingService>();
+builder.Services.AddTransient<SellerService>();
 
 var app = builder.Build();
 
 // Configurar o pipeline de requisição HTTP
 if (!app.Environment.IsDevelopment())
 {
-   
     app.UseExceptionHandler("/Home/Error");
-    // O valor padrão do HSTS é 30 dias. Você pode querer mudar isso para cenários de produção, veja https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 else
-{   // <-- este bloco inteiro de else
+{
+    // Este bloco de código só deve ser executado em ambiente de desenvolvimento
     using (var scope = app.Services.CreateScope())
     {
         var services = scope.ServiceProvider;
         var context = services.GetRequiredService<SalesWebMvcContext>();
+        context.Database.EnsureCreated(); // Cria o banco de dados se ele não existir
         var seedingService = new SeedingService(context);
         seedingService.Seed();
+
+        // Adicionar mais inicializações se necessário
+        var sellerService = services.GetRequiredService<SellerService>();
+        // Chame métodos de inicialização no sellerService, se houver
     }
 }
 
-//Popular as tabelas do sistema
-//app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
+// Popular as tabelas do sistema fora do bloco de desenvolvimento, se necessário
+// app.Services.CreateScope().ServiceProvider.GetRequiredService<SeedingService>().Seed();
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
 
 app.MapControllerRoute(
