8cf583456e3ddad1de8f9f2b6f075babeea1c116
JuanBr100 <juanrdo2009@hotmail.com>
2024-07-06 20:02:17 -0300

    Implementing grouping search

--- Services/SalesRecordService.cs
+++ Services/SalesRecordService.cs
@@ -36,5 +36,32 @@ namespace SalesWebMvc.Services
             var result = from obj in _context.SalesRecords select obj;
             return result.Min(x => x.Date);
         }
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? start, DateTime? final)
+        {
+            var query = _context.SalesRecords.AsQueryable();
+
+            if (start.HasValue)
+            {
+                query = query.Where(x => x.Date >= start.Value);
+            }
+
+            if (final.HasValue)
+            {
+                query = query.Where(x => x.Date <= final.Value);
+            }
+
+            // Execute a consulta no banco de dados e traga os dados para a memória
+            var result = await query
+                .Include(x => x.Seller)
+                .ThenInclude(s => s.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+
+            // Agora aplique o agrupamento na memória
+            var groupedResult = result.GroupBy(x => x.Seller.Department).ToList();
+
+            return groupedResult;
+        }
     }
 }
